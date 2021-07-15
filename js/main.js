52880154e767ec320ed15d2403608a60ba602c46
Daniel Liberato <danielrcliberato@gmail.com>
2021-07-15 14:46:46 -0300

    Updated main.js

--- js/main.js
+++ js/main.js
@@ -1,6 +1,8 @@
 (function (doc) {
-	"use strict"
-
+	'use strict'
+	let btnOpenMenu = document.querySelector('[data-open-menu]');
+	let btnCloseMenu = document.querySelector('[data-close-menu]');
+	let menu = document.querySelector('[data-menu]');
 	let form = doc.querySelector('[data-register]')
 	let inputName = doc.querySelector('[data-name]')
 	let inputEmail = doc.querySelector('[data-email]')
@@ -8,25 +10,37 @@
 	let modalContent = doc.querySelector('[data-content-modal]')
 	let loadingModal = doc.querySelector('[data-loading-modal]')
 	let btnCloseModal = doc.querySelector('[data-close-modal]')
-	let infoCard = doc.querySelectorAll(".info__card")
+	let infoCard = doc.querySelectorAll('.info__card')
 	let getLeads = JSON.parse(localStorage.getItem('leads'))
 
-	modal.style.display = "none"
+	modal.style.display = 'none'
+
+	function handleOpenMenu() {
+		btnOpenMenu.addEventListener('click', () => {
+			menu.style.display = 'block';
+			btnCloseMenu.style.display = 'block';
+		});
+
+		btnCloseMenu.addEventListener('click', () => {
+			menu.style.display = 'none';
+			btnCloseMenu.style.display = 'none';
+		});
+	}
 
 	function isLoading() {
 		return setTimeout(() => {
-			loadingModal.style.display = "none"
-			modalContent.style.display = "flex"
+			loadingModal.style.display = 'none'
+			modalContent.style.display = 'flex'
 		}, 3500)
 	}
 
 	const handleSubmit = (event) => {
 		event.preventDefault();
-		loadingModal.style.display = "flex"
+		loadingModal.style.display = 'flex'
 		if (inputName && inputEmail) {
 			saveLead(getLeads, inputName.value, inputEmail.value)
-			modal.style.display = "flex"
-			modalContent.style.display = "none"
+			modal.style.display = 'flex'
+			modalContent.style.display = 'none'
 			isLoading()
 		}
 	}
@@ -40,25 +54,28 @@
 	}
 
 	const handleCloseModal = () => {
-		modal.style.display = "none"
-		inputName.value = ""
-		inputEmail.value = ""
+		modal.style.display = 'none'
+		inputName.value = ''
+		inputEmail.value = ''
 		clearTimeout(isLoading)
 		// localStorage.clear()
 	}
 	btnCloseModal.addEventListener('click', handleCloseModal, false)
 
-	const handleMouseOver = () => {
-		infoCard[0].classList.remove('card')
-		card.classList.add('card')
+	function infoBoxMouseOver() {
+		infoCard.forEach(card => {
+			card.addEventListener('mouseover', () => {
+				infoCard[0].classList.remove('card')
+				card.classList.add('card')
+			}, false)
+
+			card.addEventListener('mouseout', () => {
+				card.classList.remove('card')
+				infoCard[0].classList.add('card')
+			}, false)
+		})
 	}
-	const handleMouseOut = () => {
-		card.classList.remove("card")
-		infoCard[0].classList.add('card')
-	}
-	infoCard.forEach(card => {
-		card.addEventListener("mouseover", handleMouseOver, false)
-		card.addEventListener("mouseout", handleMouseOut, false)
-	})
+	handleOpenMenu()
+	infoBoxMouseOver()
 
 })(document)
