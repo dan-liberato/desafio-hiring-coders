4417890a4ee3ea3c01404e9f1bbe284fb8108bd6
Daniel Liberato <danielrcliberato@gmail.com>
2021-07-18 12:17:49 -0300

    Updated js files

--- js/countdown.js
+++ js/countdown.js
@@ -1,30 +1,30 @@
 (function (doc) {
 	const convertDate = (time) => {
-		const total = Date.parse(time) - Date.parse(new Date())
-		const days = Math.floor(total / (1000 * 60 * 60 * 24))
-		const hours = Math.floor((total / (1000 * 60 * 60)) % 24)
-		const minutes = Math.floor((total / 1000 / 60) % 60)
-		const seconds = Math.floor((total / 1000) % 60)
-		return { total, days, hours, minutes, seconds }
+		const total = Date.parse(time) - Date.parse(new Date());
+		const days = Math.floor(total / (1000 * 60 * 60 * 24));
+		const hours = Math.floor((total / (1000 * 60 * 60)) % 24);
+		const minutes = Math.floor((total / 1000 / 60) % 60);
+		const seconds = Math.floor((total / 1000) % 60);
+		return { total, days, hours, minutes, seconds };
 	}
 
 	const countdown = (element, time) => {
-		const timer = doc.querySelector(element)
-		const days = timer.querySelector('.card .days')
-		const hours = timer.querySelector('.card .hours')
-		const minutes = timer.querySelector('.card .minutes')
-		const seconds = timer.querySelector('.card .seconds')
+		const timer = doc.querySelector(element);
+		const days = timer.querySelector('.card .days');
+		const hours = timer.querySelector('.card .hours');
+		const minutes = timer.querySelector('.card .minutes');
+		const seconds = timer.querySelector('.card .seconds');
 
 		function updateTime() {
-			const countdown = convertDate(time)
-			days.innerHTML = countdown.days
-			hours.innerHTML = countdown.hours
-			minutes.innerHTML = countdown.minutes
-			seconds.innerHTML = countdown.seconds
-			if (countdown.total <= 0) clearInterval(timeInterval)
+			const countdown = convertDate(time);
+			days.innerHTML = countdown.days;
+			hours.innerHTML = countdown.hours;
+			minutes.innerHTML = countdown.minutes;
+			seconds.innerHTML = countdown.seconds;
+			if (countdown.total <= 0) clearInterval(timeInterval);
 		}
-		updateTime()
-		const timeInterval = setInterval(updateTime, 1000)
+		updateTime();
+		const timeInterval = setInterval(updateTime, 1000);
 	}
-	countdown('#countdown', '2021-11-26')
-})(document)
+	countdown('#countdown', '2021-11-26');
+})(document);
--- js/main.js
+++ js/main.js
@@ -1,19 +1,19 @@
 (function (doc, win) {
 	'use strict'
 	let menu = document.querySelector('[data-menu]');
-	let menuLink = doc.querySelectorAll('.menu__list__link')
+	let menuLink = doc.querySelectorAll('.menu__list__link');
 	let btnCloseMenu = document.querySelector('[data-close-menu]');
-	let form = doc.querySelector('[data-register]')
-	let inputName = doc.querySelector('[data-name]')
-	let inputEmail = doc.querySelector('[data-email]')
-	let modal = doc.querySelector('[data-modal]')
-	let modalContent = doc.querySelector('[data-content-modal]')
-	let loadingModal = doc.querySelector('[data-loading-modal]')
-	let btnCloseModal = doc.querySelector('[data-close-modal]')
-	let infoCard = doc.querySelectorAll('.info__card')
-	let getLeads = JSON.parse(localStorage.getItem('leads'))
+	let form = doc.querySelector('[data-register]');
+	let inputName = doc.querySelector('[data-name]');
+	let inputEmail = doc.querySelector('[data-email]');
+	let modal = doc.querySelector('[data-modal]');
+	let modalContent = doc.querySelector('[data-content-modal]');
+	let loadingModal = doc.querySelector('[data-loading-modal]');
+	let btnCloseModal = doc.querySelector('[data-close-modal]');
+	let infoCard = doc.querySelectorAll('.info__card');
+	let getLeads = JSON.parse(localStorage.getItem('leads'));
 
-	modal.style.display = 'none'
+	modal.style.display = 'none';
 
 	function handleClickMenu() {
 		let btnOpenMenu = document.querySelector('[data-open-menu]');
@@ -36,57 +36,57 @@
 					menu.style.display = 'none';
 					btnCloseMenu.style.display = 'none';
 				}, false);
-			})
+			});
 		}
 	}
 
 	function isLoading() {
 		return setTimeout(() => {
-			loadingModal.style.display = 'none'
-			modalContent.style.display = 'flex'
-		}, 3500)
+			loadingModal.style.display = 'none';
+			modalContent.style.display = 'flex';
+		}, 3500);
 	}
 
 	const saveLead = (data, name, email) => {
-		const leads = []
-		if (data) leads.push({ name, email }, ...data)
-		if (!data) leads.push({ name, email })
-		localStorage.setItem('leads', JSON.stringify(leads))
+		const leads = [];
+		if (data) leads.push({ name, email }, ...data);
+		if (!data) leads.push({ name, email });
+		localStorage.setItem('leads', JSON.stringify(leads));
 	}
 
 	const handleSubmit = (event) => {
 		event.preventDefault();
-		loadingModal.style.display = 'flex'
-		saveLead(getLeads, inputName.value, inputEmail.value)
-		modal.style.display = 'flex'
-		modalContent.style.display = 'none'
-		isLoading()
+		loadingModal.style.display = 'flex';
+		saveLead(getLeads, inputName.value, inputEmail.value);
+		modal.style.display = 'flex';
+		modalContent.style.display = 'none';
+		isLoading();
 	}
-	form.addEventListener('submit', handleSubmit, false)
+	form.addEventListener('submit', handleSubmit, false);
 
 	const handleCloseModal = () => {
-		modal.style.display = 'none'
-		inputName.value = ''
-		inputEmail.value = ''
-		clearTimeout(isLoading)
+		modal.style.display = 'none';
+		inputName.value = '';
+		inputEmail.value = '';
+		clearTimeout(isLoading);
 		// localStorage.clear()
 	}
-	btnCloseModal.addEventListener('click', handleCloseModal, false)
+	btnCloseModal.addEventListener('click', handleCloseModal, false);
 
 	function infoBoxMouseOver() {
 		infoCard.forEach(card => {
 			card.addEventListener('mouseover', () => {
-				infoCard[0].classList.remove('card')
-				card.classList.add('card')
-			}, false)
+				infoCard[0].classList.remove('card');
+				card.classList.add('card');
+			}, false);
 
 			card.addEventListener('mouseout', () => {
-				card.classList.remove('card')
-				infoCard[0].classList.add('card')
-			}, false)
-		})
+				card.classList.remove('card');
+				infoCard[0].classList.add('card');
+			}, false);
+		});
 	}
-	handleClickMenu()
-	handleClickMenuItem()
-	infoBoxMouseOver()
-})(document, window)
+	handleClickMenu();
+	handleClickMenuItem();
+	infoBoxMouseOver();
+})(document, window);
